598613d74c33bd49051f26873c642b8f57e57341
JankovicN <nj20180199@student.fon.bg.ac.rs>
2022-04-04 13:29:45 +0200

    Kreirana klasa Main3 koja cita podatke iz JSON fajla niz _objekata i upisuje ih u listu objekata klase Bored, zatim ispisuje listu

---
+++ json-api-caller/src/main/java/rs/ac/bg/fon/np/main/Main3.java
@@ -0,0 +1,31 @@
+package rs.ac.bg.fon.np.main;
+
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import rs.ac.bg.fon.np.domain.Bored;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		
+		try(FileReader in = new FileReader("niz_objekata.json")){
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			
+			List<Bored> bored = Arrays.asList(gson.fromJson(in, Bored[].class));
+			
+			for(Bored b: bored) {
+				System.out.println(b);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
